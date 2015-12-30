43d24c8e28f45a9299765979eaad57d1bf3db484
Ronglin <ronglin@kooboo.com>
2015-12-30 14:59:34 +0800

    initial

---
+++ DQueue.WinTest/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace DQueue.WinTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSendMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtReceiveMessage = new System.Windows.Forms.TextBox();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtSendMessage
+            // 
+            this.txtSendMessage.Location = new System.Drawing.Point(12, 12);
+            this.txtSendMessage.Multiline = true;
+            this.txtSendMessage.Name = "txtSendMessage";
+            this.txtSendMessage.Size = new System.Drawing.Size(348, 178);
+            this.txtSendMessage.TabIndex = 0;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(251, 210);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(109, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send Message";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtReceiveMessage
+            // 
+            this.txtReceiveMessage.Location = new System.Drawing.Point(417, 12);
+            this.txtReceiveMessage.Multiline = true;
+            this.txtReceiveMessage.Name = "txtReceiveMessage";
+            this.txtReceiveMessage.Size = new System.Drawing.Size(348, 178);
+            this.txtReceiveMessage.TabIndex = 2;
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(656, 210);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(109, 23);
+            this.btnReceive.TabIndex = 3;
+            this.btnReceive.Text = "Receive Message";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 258);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.txtReceiveMessage);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSendMessage);
+            this.Name = "Form1";
+            this.Text = "MQ TEST";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSendMessage;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtReceiveMessage;
+        private System.Windows.Forms.Button btnReceive;
+    }
+}
+
---
+++ DQueue/Core/IQueue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DQueue.Core
+{
+    public interface IQueue
+    {
+        void Send(IMessage message);
+
+        IMessage Receive();
+    }
+}
