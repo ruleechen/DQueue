f0191aaee0d89a0084189c8af06a9ca9520a4dfe
Rulee <rulee@live.cn>
2016-01-02 03:19:51 +0800

    update

--- DQueue/Interfaces/IQueueMessage.cs
+++ DQueue/Interfaces/IQueueMessage.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace DQueue.Interfaces
 {
     public interface IQueueMessage
     {
+        [JsonIgnore]
         string QueueName { get; }
     }
 }
