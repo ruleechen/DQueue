54fc82db1e0d4dc1081eb6e6d5767ccef8af52a8
Rulee <rulee@live.cn>
2016-01-06 00:36:55 +0800

    create tests

---
+++ DQueue.WinTest/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DQueue.WinTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.txtReceive = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(161, 198);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(12, 12);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(237, 165);
+            this.txtSend.TabIndex = 2;
+            // 
+            // txtReceive
+            // 
+            this.txtReceive.Location = new System.Drawing.Point(281, 12);
+            this.txtReceive.Multiline = true;
+            this.txtReceive.Name = "txtReceive";
+            this.txtReceive.Size = new System.Drawing.Size(237, 165);
+            this.txtReceive.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 238);
+            this.Controls.Add(this.txtReceive);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "DQueue";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.TextBox txtReceive;
+    }
+}
+
---
+++ DQueue.WinTest/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DQueue.WinTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            var control = this.txtReceive;
+
+            Task.Factory.StartNew(() =>
+            {
+                var consumer = new QueueConsumer();
+
+                consumer.Receive<SampleMessage>((message) =>
+                {
+                    control.Text = message.FirstName;
+                });
+            });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            var producer = new QueueProducer();
+
+            var text = this.txtSend.Text;
+
+            producer.Send(new SampleMessage
+            {
+                FirstName = text,
+                LastName = text
+            });
+
+        }
+    }
+}
