c2fef06d2162daad2b904fb185f479dff9810850
Rulee <rulee@live.cn>
2016-01-13 00:03:44 +0800

    add IQueueService

---
+++ DQueue/Interfaces/IQueueService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DQueue.Interfaces
+{
+    public interface IQueueService
+    {
+        void Start(string[] args);
+
+        void Stop();
+    }
+}
