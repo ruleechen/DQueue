8fa189a1bc515f8db8637b333ffc772069ece6da
Rulee <rulee@live.cn>
2016-01-16 23:33:30 +0800

    update

--- DQueue/Interfaces/DispatchContext.cs
+++ DQueue/Interfaces/DispatchContext.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +10,24 @@ namespace DQueue.Interfaces
 {
     public class DispatchContext
     {
+        private IDictionary _items;
+
         private Action<DispatchContext, DispatchStatus> _action;
         private CancellationToken _token;
         private List<Exception> _exceptions;
+
+        private bool _isCompleted;
         private object _exceptionsLocker;
 
         public DispatchContext(CancellationToken token, Action<DispatchContext, DispatchStatus> action)
         {
+            _items = Hashtable.Synchronized(new Hashtable()); // thread safe
+
             _token = token;
             _action = action;
             _exceptions = new List<Exception>();
+
+            _isCompleted = false;
             _exceptionsLocker = new object();
         }
 
@@ -30,16 +39,31 @@ namespace DQueue.Interfaces
             }
         }
 
+        public IDictionary Items
+        {
+            get
+            {
+                return _items;
+            }
+        }
+
         public void Complete()
         {
+            _isCompleted = true;
             _action(this, DispatchStatus.Complete);
         }
 
         public void LogException(Exception ex)
         {
-            lock (_exceptionsLocker)
+            if (!_isCompleted)
             {
-                _exceptions.Add(ex);
+                lock (_exceptionsLocker)
+                {
+                    if (!_isCompleted)
+                    {
+                        _exceptions.Add(ex);
+                    }
+                }
             }
         }
 
